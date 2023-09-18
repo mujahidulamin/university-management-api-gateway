50c209338f55f641ef627a22d7c5ffe6565bb50d
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2023-09-19 04:20:33 +0600

    first commit

---
+++ src/shared/redis.ts
@@ -0,0 +1,19 @@
+import { createClient } from 'redis';
+import config from '../config';
+import logger from './logger';
+
+let redisClient = createClient({
+  //   url: config.redis.url
+  url: 'redis://localhost:6379'
+});
+
+redisClient.on('error', (err) => logger.error('RedisError', err));
+redisClient.on('connect', (err) => logger.info('Redis connected'));
+
+const connect = async (): Promise<void> => {
+  await redisClient.connect();
+};
+
+export const RedisClient = {
+  connect
+};
