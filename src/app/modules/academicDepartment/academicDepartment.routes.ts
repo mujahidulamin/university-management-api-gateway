cca887ebb62e075fbd2054b6b696aa30dbf851b7
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2023-09-24 17:44:09 +0600

    api gateway done

---
+++ src/app/modules/academicDepartment/academicDepartment.routes.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import { AcademicDepartmentController } from './academicDepartment.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicDepartmentValidation } from './academicDepartment.validations';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
+
+const router = express.Router();
+
+router.get('/', AcademicDepartmentController.getAllFromDB);
+
+router.get('/:id', AcademicDepartmentController.getByIdFromDB);
+
+router.post(
+  '/',
+  validateRequest(AcademicDepartmentValidation.create),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicDepartmentController.insertIntoDB
+);
+
+router.patch(
+  '/:id',
+  validateRequest(AcademicDepartmentValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicDepartmentController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicDepartmentController.deleteByIdFromDB
+);
+
+export const academicDepartmentRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,13 +1,103 @@
 import express from 'express';
-import { academicSemesterRoutes } from '../modules/academicSemester.routes';
+import { academicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
+import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
+import { academicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
+import { buildingRoutes } from '../modules/building/building.routes';
+import { roomRoutes } from '../modules/room/room.routes';
+import { courseRoutes } from '../modules/course/course.routes';
+import { offeredCourseRoutes } from '../modules/offeredCourse/offeredCourse.routes';
+import { semesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.routes';
+import { offeredCourseSectionRoutes } from '../modules/offeredCourseSection/offeredCourseSection.routes';
+import { offeredCourseClassScheduleRoutes } from '../modules/offeredCourseClassSchedule/offeredCourseClassSchedule.routes';
+import { studentEnrolledCourseRoutes } from '../modules/studentEnrolledCourse/studentEnrolledCourse.routes';
+import { studentEnrolledCourseMarkRoutes } from '../modules/studentEnrolledCourseMark/studentEnrolledCourseMark.routes';
+import { studentSemesterPaymentRoutes } from '../modules/studentSemesterPayment/studentSemesterPayment.routes';
+import { adminRoutes } from '../modules/admin/admin.routes';
+import { authRoutes } from '../modules/auth/auth.routes';
+import { UserRoutes } from '../modules/user/user.routes';
+import { studentRoutes } from '../modules/student/student.routes';
+import { facultyRoutes } from '../modules/faculty/faculty.routes';
+import { managementDepartmentRoutes } from '../modules/managementDepartment/managementDepartment.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
   {
+    path: '/users',
+    routes: UserRoutes
+  },
+  {
+    path: '/auth',
+    routes: authRoutes
+  },
+  {
+    path: '/students',
+    routes: studentRoutes
+  },
+  {
+    path: '/faculties',
+    routes: facultyRoutes
+  },
+  {
+    path: '/academic-faculties',
+    routes: academicFacultyRoutes
+  },
+  {
     path: '/academic-semesters',
     routes: academicSemesterRoutes
   },
+  {
+    path: '/academic-departments',
+    routes: academicDepartmentRoutes
+  },
+  {
+    path: '/buildings',
+    routes: buildingRoutes
+  },
+  {
+    path: '/rooms',
+    routes: roomRoutes
+  },
+  {
+    path: '/courses',
+    routes: courseRoutes
+  },
+  {
+    path: '/semester-registrations',
+    routes: semesterRegistrationRoutes
+  },
+  {
+    path: '/offered-courses',
+    routes: offeredCourseRoutes
+  },
+  {
+    path: '/offered-course-sections',
+    routes: offeredCourseSectionRoutes
+  },
+  {
+    path: '/offered-course-class-schedules',
+    routes: offeredCourseClassScheduleRoutes
+  },
+  {
+    path: '/student-enrolled-courses',
+    routes: studentEnrolledCourseRoutes
+  },
+  {
+    path: '/student-enrolled-course-marks',
+    routes: studentEnrolledCourseMarkRoutes
+  },
+  {
+    path: '/student-semester-payments',
+    routes: studentSemesterPaymentRoutes
+  },
+  {
+    path: '/management-departments',
+    routes: managementDepartmentRoutes
+  },
+  {
+    path: '/admins',
+    routes: adminRoutes
+  }
 ];
 
 moduleRoutes.forEach((route) => {
